86958ee38453b9d1897aae26d41df010c7098384
niraj99 <nirajsahni04@gmail.com>
2021-12-09 00:21:59 +0530

    API assignment

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,21 +1,59 @@
-var input = document.getElementsByTagName('input')[0];
-var btn = document.getElementsByTagName('button')[0];
-var toDolist = document.getElementsByClassName('todos')[0];
-var count = 0;
+const com = document.querySelector("#completed");
+const pen = document.querySelector("#pending");
+const divTodo = document.querySelector(".todos");
 
-btn.addEventListener('click', () => {
-    var para = document.createElement('p');
-    para.setAttribute("key", count);
-    count += 1;
-    para.innerHTML = input.value;
-    toDolist.appendChild(para);
-    input.value = "";
 
-    para.addEventListener("click", () => {
-        toDolist.removeChild(para);
-    });
+fetch("https://jsonplaceholder.typicode.com/todos", {
+    "method" : "GET"
+})
+.then(response => {
+    return response.json();
+})
+.then(data =>{
+    const dataArr = data;
+    var completedArr = [];
+    var pendingArr = [];
+    for(var i = 0; i<dataArr.length; i++){
+        if(dataArr[i]["completed"] === true){
+            completedArr.push(dataArr[i]["title"]);
+        }else{
+            pendingArr.push(dataArr[i]["title"]);
+        }
+    }
+    com.addEventListener("click", function(){
+        document.querySelectorAll(".pending").forEach(e => e.remove());
+        
+        for(let i =0; i<completedArr.length; i++){
+            var para = document.createElement("p");
+            para.setAttribute("class", "comp");
+            para.innerText = completedArr[i];
+            divTodo.append(para);
+
+        }
+        
+        
+    })
+
+    pen.addEventListener("click", function(){
+        document.querySelectorAll(".comp").forEach(e => e.remove());
+
+        for(let i =0; i<pendingArr.length; i++){
+            var para = document.createElement("p");
+            para.setAttribute("class", "pending")
+            para.innerText = pendingArr[i];
+            divTodo.append(para);
+        }
+        
+    })
     
-});
+    
+})
+.catch(err => {
+    console.error(err);
+})
+
+
+
 
 
 
